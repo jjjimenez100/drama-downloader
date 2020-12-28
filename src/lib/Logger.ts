ab0c5c4facb36ad760518db7e48a1310ef840ec8
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-12-28 23:38:23 +0800

    fix: remove unnecessary api boilerplate files

--- src/lib/Logger.ts
+++
@@ -1,7 +0,0 @@
-interface Logger {
-    info(message: string, ...params: string[]): void;
-    debug(message: string, ...params: string[]): void;
-    error(message: string, ...params: string[]): void;
-}
-
-export default Logger;
